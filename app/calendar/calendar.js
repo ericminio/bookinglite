534721578eba0164c74e1d061bca683df2467717
mathieu <mathieu@domainesnordik.com>
2016-09-20 22:09:50 -0400

    adding day dynamically

--- app/calendar/calendar.js
+++ app/calendar/calendar.js
@@ -6,31 +6,34 @@ function Calendar(date) {
   this.date = date;
 }
 
-Calendar.prototype.fillCalendar = function(page, currentDate) {
-  var calendarSection = page("#calendar");
-  calendarSection.empty();
-  
-  calendarSection.append('<table>');
-  this.fillMonth(calendarSection);
-  
-  calendarSection.append('</table>');
+Calendar.prototype.fillCalendar = function(page) {
+
+  var table = cheerio('<table> </table>');
+  table.append(this.buildMonthRow());
+  table.append(this.buildDayRow());
+  page("#calendar").empty().append(table);
+
   
 };
 
-Calendar.prototype.fillMonth = function(calendarSection){
+Calendar.prototype.buildMonthRow = function(){
   var daysInMonth = dateUtils.daysInMonth(this.date);
-  calendarSection.append('<tr class=month> <td colspan'+daysInMonth+'>'+dateUtils.getMonth(this.date)+'</td> </tr>');
+  return cheerio('<tr class=month> <td colspan'+daysInMonth+'>'+dateUtils.getMonth(this.date)+'</td> </tr>');
 };
 
-Calendar.prototype.fillDay = function(calendarSection){
+
+Calendar.prototype.buildDayRow = function(calendarSection){
   var daysInMonth = dateUtils.daysInMonth(this.date);
   
-  calendarSection.append('<tr class=day>');
+  var newTr = cheerio('<tr class=day> </tr>');
   for(i=0;i<daysInMonth;i++){
     var currentDate = new Date(this.date.getFullYear(), this.date.getMonth(), i,0,0,0,0);
-    calendarSection.append('<td class=weekday'+currentDate.getDay()+'>'+i+'</td>');
+    
+    var newTd = cheerio('<td class=weekday'+currentDate.getDay()+'>'+i+'</td>');
+    newTr.append(newTd);
   }
-  calendarSection.append('</tr>');
+
+  return newTr;
 };
 
 
--- app/homepage.js
+++ app/homepage.js
@@ -1,14 +1,19 @@
 var fs        = require('fs');
 var cheerio		= require('cheerio');
-var fillCalendar = require('./calendar/calendar');
+var Calendar = require('./calendar/calendar');
 
 homepage = function(request, response, database) {
   var html = fs.readFileSync('./app/homepage.html').toString();
   var $ = cheerio.load(html);
 	
-	var calendar = cheerio.load(fs.readFileSync('./app/calendar/calendar.html').toString())('#calendar').html();
+	var tempDate = new Date(2016, 8, 19, 0,0,0,0);
+	var calendar = new Calendar(tempDate);
 	
-	fillCalendar($);
+	//var calendarSection = cheerio.load(fs.readFileSync('./app/calendar/calendar.html').toString())('#calendar').html();
+	
+	$("#calendar").empty().html();
+	
+	calendar.fillCalendar($);
 	
   response.write($.html());
 	response.end();
